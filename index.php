5f3875117c2aaed4b5fe3b1dd9825a402754ba39
Your Name <you@example.com>
2020-02-03 13:49:30 +0100

    hello world 3

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <body>
         // Hier echo ik iets op mijn scherm
         <?php echo "<h1>Dit is mijn website</h1>"; ?>
+        <img src="assets/images/download.jpg">
     </body>
 </html>
 
