bb868ef0e3e32dd14937cd9a720a3f04aecd78d5
Your Name <you@example.com>
2020-02-10 10:59:30 +0100

    nieuw echo toegevoegd!

---
+++ naam.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <?php 
+        $naam ='Thomas';
+        $leeftijd = 21;
+        $lengte =1.85;
+        echo $naam;  
+    ?>
+    <?php
+    echo "ik ben " .$naam . ", ik ben ".$leeftijd .  ' jaar' . " oud en ben " ,$lengte . 'm lang'; 
+    ?> 
+</body>
+</html>
+
+
+    
